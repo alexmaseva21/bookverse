d33aa4f0a079d2c3fe88a70aed0714db467071e6
ASUS <alexmaseva@gmail.com>
2026-06-20 00:45:45 +0300

    feat: implement home controller mapping and populate index view

---
+++ src/main/java/web/HomeController.java
@@ -0,0 +1,14 @@
+package web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        // This tells Spring to look for a file named "index.html" inside src/main/resources/templates/
+        return "index";
+    }
+}
